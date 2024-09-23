139010aefe8fdc9fd595b1f76a85c16fc643c220
HarperDoug <73361907+HarperDoug@users.noreply.github.com>
2024-09-23 17:29:00 +0100

    changed Functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-function citySearch(event) {
+function searchCity(city) {
+  let apiKey = "5fftb729a03o1b248f0ff72a8832d43e";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiURL);
+}
+
+function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-name-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", citySearch);
+searchFormElement.addEventListener("submit", submitSearch);
