c753f22e8497620fa079d6cdd6ebf4fef86c943d
HarperDoug <73361907+HarperDoug@users.noreply.github.com>
2024-09-27 14:11:13 +0100

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+function getWeatherData(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp-value");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let humidityElement = document.querySelector("#humidity-value");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind-value");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let pressureElement = document.querySelector("#pressure-value");
+  pressureElement.innerHTML = response.data.temperature.pressure;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(city) {
   let apiKey = "5fftb729a03o1b248f0ff72a8832d43e";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiURL);
+  axios.get(apiURL).then(getWeatherData);
 }
 
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-name-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
+
+searchCity("London");
